15622c457198d2112fc9c89cb53d4da74d6a3858
larry taylor and nicole freed <larry@>
2017-09-13 13:48:52 -0700

    build App with Tamagotchi and NewTamagotchi components

---
+++ src/components/App.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import NewTamagotchi from " ./NewTamagotchi";
+import Tamagotchi from "./Tamagotchi";
+
+function App(){
+  return (
+    <div>
+      <NewTamagotchi/>
+      <Tamagotchi/>
+    </div>
+  );
+}
+
+export default App;
