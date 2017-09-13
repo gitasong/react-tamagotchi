6aa8210f351cc8b92cc1f5bffc85b8693a407d60
larry taylor and nicole freed <larry@>
2017-09-13 13:56:23 -0700

    create Tamagotchi model

---
+++ src/models/Tamagotchi.js
@@ -0,0 +1,11 @@
+class Tamagotchi {
+  constructor(name, isSleeping, sleep, food, play) {
+    this.name = name;
+    this.isSleeping = false;
+    this.sleep = sleep;
+    this.food = food;
+    this.play = play;
+  }
+}
+
+export default Tamagotchi;
