345271bb2c905c9e752f2deddd016dd3190a4074
Larry Taylor and Nicole Freed <larry@>
2017-09-14 14:28:47 -0700

    refactor into class-based component

--- src/components/NewTamagotchi.jsx
+++ src/components/NewTamagotchi.jsx
@@ -3,24 +3,31 @@ import Tamagotchi from "../models/Tamagotchi.js";
 import PropTypes from "prop-types";
 import { Button } from "react-bootstrap";
 
-function NewTamagotchi(props) {
-  return (
-    <div>
-      <form>
-        <input
-          ref="_name"
-          type="text"
-          id="name"
-          placeholder="Tamagotchi name" />
+class NewTamagotchi extends React.Component {
+
+  constructor(props) {
+    super(props);
+  }
+
+  render () {
+    return (
+      <div>
+        <form>
           <input
-            ref="_image"
+            ref="_name"
             type="text"
-            id="image"
-            placeholder="Tamagotchi avatar URL" />
-          <Button bsStyle="success" type="submit">Create New Tamagotchi</Button>
-      </form>
-    </div>
-  );
+            id="name"
+            placeholder="Tamagotchi name" />
+            <input
+              ref="_image"
+              type="text"
+              id="image"
+              placeholder="Tamagotchi avatar URL" />
+            <Button bsStyle="success" type="submit">Create New Tamagotchi</Button>
+        </form>
+      </div>
+    );
+  }
 }
 
 export default NewTamagotchi;
